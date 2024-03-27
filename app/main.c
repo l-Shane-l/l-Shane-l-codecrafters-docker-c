d59084099df01a68cb91c2393b381112aeebb2f0
l-Shane-l <graainneog@proton.me>
2024-03-27 21:55:07 +0100

    stage 6

--- app/main.c
+++ app/main.c
@@ -1,101 +1,80 @@
-#include <dirent.h>
+#define _GNU_SOURCE
 #include <fcntl.h>
+#include <sched.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-// function to create directories on unix systems
-void mk_dir(char *temp_dir_path, char *command_path) {
-  for (int i = strlen(temp_dir_path) + 1; command_path[i] != '\0'; i++) {
-    if (command_path[i] == '/') {
-      command_path[i] = '\0';
-      if (mkdir(command_path, 0777) < 0) {
-        printf("cannot make directory %s\n", command_path);
-        perror("mkdir");
-      }
-      command_path[i] = '/';
-    }
-  }
+void copy_file(const char *from, const char *to) {
+  char cmd[1024];
+  snprintf(cmd, sizeof(cmd), "cp %s %s", from, to);
+  system(cmd);
 }
 
-// basic cp function
-void copy_files(char *from_path, char *to_path) {
-  int from;
-  int to;
-  char buffer[4096];
-  int ret;
-
-  from = open(from_path, O_RDONLY);
-  if (from < 0) {
-    perror("error open from path");
-    exit(1);
-  }
-
-  to = open(to_path, O_WRONLY | O_CREAT | O_EXCL, 0777);
-  if (to < 0) {
-    perror("error open to path");
-    exit(1);
-  }
-
-  while ((ret = read(from, buffer, 4096)) > 0) {
-    if (ret < 0 || write(to, buffer, ret) < 0) {
-      perror("error write");
-      exit(1);
-    }
-  }
-
-  close(from);
-  close(to);
-}
-
-void createDirContainer(char *command, char *temp_dir_path) {
-  char command_path[4096];
-  char *n = mkdtemp(temp_dir_path);
-  if (n == NULL) {
-    printf("Cannot create temp directory.\n");
-    exit(1);
-  }
-
-  sprintf(command_path, "%s%s", temp_dir_path, command);
-  mk_dir(temp_dir_path, command_path);
-  copy_files(command, command_path);
-}
-
-// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 int main(int argc, char *argv[]) {
   // Disable output buffering
   setbuf(stdout, NULL);
 
-  char *command = argv[3];
+  int pipe_out[2];
+  pipe(pipe_out);
+  int pipe_err[2];
+  pipe(pipe_err);
+
+  char *command = "./sandbox/ex";
+  mkdir("./sandbox", 0777);
+
+  copy_file(argv[3], command);
+
+  chroot("./sandbox");
+  unshare(CLONE_NEWPID);
+
   int child_pid = fork();
+
   if (child_pid == -1) {
     printf("Error forking!");
     return 1;
   }
 
   if (child_pid == 0) {
-    // Replace current program with calling program
-    char temp_dir_path[] = "/tmp/container_XXXXXX";
-    createDirContainer(command, temp_dir_path);
+    // pipe stdout to parent
+    close(pipe_err[0]);
+    close(pipe_out[0]);
 
-    if (chroot(temp_dir_path) != 0) {
-      perror("chroot");
-      exit(1);
-    }
+    dup2(pipe_out[1], STDOUT_FILENO);
+    dup2(pipe_err[1], STDERR_FILENO);
 
     execv(command, &argv[3]);
-    perror("ERR: execv");
+
+    close(pipe_out[1]);
+    close(pipe_err[1]);
+
   } else {
-    // We're in parent
-    int status;
-    waitpid(child_pid, &status, 0);
-    if (WIFEXITED(status)) {
-      return WEXITSTATUS(status);
+    close(pipe_out[1]);
+    close(pipe_err[1]);
+
+    char buffer[1024];
+    size_t len = 0;
+
+    while ((len = read(pipe_out[0], buffer, sizeof(buffer))) > 0) {
+      write(STDOUT_FILENO, buffer, len);
     }
-    // printf("Child terminated");
+
+    while ((len = read(pipe_err[0], buffer, sizeof(buffer))) > 0) {
+      write(STDERR_FILENO, buffer, len);
+    }
+
+    close(pipe_out[0]);
+    close(pipe_err[0]);
+
+    int child_status = 0;
+    wait(&child_status);
+    exit(WEXITSTATUS(child_status));
   }
 
   return 0;
