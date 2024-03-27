ef6830948b5fcd2edf4e7b7bad1903b95fe95946
l-Shane-l <graainneog@proton.me>
2024-03-27 21:43:39 +0100

    stage 5

--- README.md
+++ README.md
@@ -18,3 +18,28 @@ docker from scratch following the codecrafters challenge, this is an educational
   In the end I had to look up the solution, I nearly
   had the answer but failed to return the correct
   value at the end of the if statement.
+
+### Stage 4
+
+- Im really struggling now and get the feeling
+  a C developer would have no issues here.
+  In the end I had to use one of the examples provided
+  I tried using the command line tools like.
+  cp and mkdir but It wasnt giving the expected
+  output for the tests.
+
+- In the end I get the feeling, for me at least
+  this would be much easier to do in a bash script
+  using command line tools. This is probably
+  due to my lack of familiarity with
+  C unix programming.
+
+- overall as much as im learning here
+  much of it is just a respect for C programming
+  I can see clearly the gulp in my understanding
+  and what is required to be proficient.
+
+- I also get the feeling this is a simply task
+  in C, just spawning a process, attaching pipes,
+  making it secure chroot. I know this would be
+  very straight forward to accomplish in bash
--- app/main.c
+++ app/main.c
@@ -1,90 +1,101 @@
+#include <dirent.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
+// function to create directories on unix systems
+void mk_dir(char *temp_dir_path, char *command_path) {
+  for (int i = strlen(temp_dir_path) + 1; command_path[i] != '\0'; i++) {
+    if (command_path[i] == '/') {
+      command_path[i] = '\0';
+      if (mkdir(command_path, 0777) < 0) {
+        printf("cannot make directory %s\n", command_path);
+        perror("mkdir");
+      }
+      command_path[i] = '/';
+    }
+  }
+}
+
+// basic cp function
+void copy_files(char *from_path, char *to_path) {
+  int from;
+  int to;
+  char buffer[4096];
+  int ret;
+
+  from = open(from_path, O_RDONLY);
+  if (from < 0) {
+    perror("error open from path");
+    exit(1);
+  }
+
+  to = open(to_path, O_WRONLY | O_CREAT | O_EXCL, 0777);
+  if (to < 0) {
+    perror("error open to path");
+    exit(1);
+  }
+
+  while ((ret = read(from, buffer, 4096)) > 0) {
+    if (ret < 0 || write(to, buffer, ret) < 0) {
+      perror("error write");
+      exit(1);
+    }
+  }
+
+  close(from);
+  close(to);
+}
+
+void createDirContainer(char *command, char *temp_dir_path) {
+  char command_path[4096];
+  char *n = mkdtemp(temp_dir_path);
+  if (n == NULL) {
+    printf("Cannot create temp directory.\n");
+    exit(1);
+  }
+
+  sprintf(command_path, "%s%s", temp_dir_path, command);
+  mk_dir(temp_dir_path, command_path);
+  copy_files(command, command_path);
+}
+
+// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 int main(int argc, char *argv[]) {
   // Disable output buffering
   setbuf(stdout, NULL);
 
-  if (argc < 4) {
-    fprintf(stderr, "Usage: %s run <image> <command> <arg1> <arg2> ...\n",
-            argv[0]);
-    exit(EXIT_FAILURE);
-  }
-
-  int stdout_pipe[2];
-  int stderr_pipe[2];
-  char buffer[4096];
-
-  // Create pipes for stdout and stderr
-  if (pipe(stdout_pipe) == -1 || pipe(stderr_pipe) == -1) {
-    perror("pipe");
-    exit(EXIT_FAILURE);
-  }
-
   char *command = argv[3];
   int child_pid = fork();
   if (child_pid == -1) {
-    perror("fork");
-    exit(EXIT_FAILURE);
+    printf("Error forking!");
+    return 1;
   }
 
   if (child_pid == 0) {
-    // Child process
+    // Replace current program with calling program
+    char temp_dir_path[] = "/tmp/container_XXXXXX";
+    createDirContainer(command, temp_dir_path);
 
-    // Close the read-end of the pipes in the child
-    close(stdout_pipe[0]);
-    close(stderr_pipe[0]);
+    if (chroot(temp_dir_path) != 0) {
+      perror("chroot");
+      exit(1);
+    }
 
-    // Redirect stdout to the write-end of the stdout pipe
-    dup2(stdout_pipe[1], STDOUT_FILENO);
-    // Redirect stderr to the write-end of the stderr pipe
-    dup2(stderr_pipe[1], STDERR_FILENO);
-
-    // Execute the command
     execv(command, &argv[3]);
-
-    // execv only returns if there is an error
-    perror("execv");
-    exit(EXIT_FAILURE);
+    perror("ERR: execv");
   } else {
-    // Parent process
-
-    // Close the write-end of the pipes in the parent
-    close(stdout_pipe[1]);
-    close(stderr_pipe[1]);
-
+    // We're in parent
     int status;
-    int child_exit_code = -1;
-
-    // wait child process exit
     waitpid(child_pid, &status, 0);
-
     if (WIFEXITED(status)) {
-      // child process exit normal
-      child_exit_code = WEXITSTATUS(status);
+      return WEXITSTATUS(status);
     }
-
-    // Read from stdout pipe
-    int bytes_read = read(stdout_pipe[0], buffer, sizeof(buffer) - 1);
-    if (bytes_read > 0) {
-      buffer[bytes_read] = '\0'; // Null-terminate the string
-      printf("%s", buffer);
-    }
-
-    // Read from stderr pipe
-    bytes_read = read(stderr_pipe[0], buffer, sizeof(buffer) - 1);
-    if (bytes_read > 0) {
-      buffer[bytes_read] = '\0'; // Null-terminate the string
-      fprintf(stderr, "%s", buffer);
-    }
-
-    // Close the read-end of the pipes
-    close(stdout_pipe[0]);
-    close(stderr_pipe[0]);
-
-    return child_exit_code;
+    // printf("Child terminated");
   }
 
   return 0;
